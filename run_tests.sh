567f0d9ee157ae6e6be28559d0153efdabe4b394
Dmitriy Chernow <dmitriychernow2000@gmail.com>
2023-05-31 14:37:48 +0300

    test docker container down after test's completed

--- run_tests.sh
+++ run_tests.sh
@@ -5,3 +5,7 @@ cd infrastructures/config/test || exit
 docker-compose -f docker-compose.yml down
 
 docker-compose -f docker-compose.yml up -d --build
+
+docker exec -it test_app pytest
+
+docker-compose -f docker-compose.yml down
